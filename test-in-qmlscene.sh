1c7727a76302351a6e398c956d87823ad92e5be0
eLtMosen <koennecke@mosushi.com>
2022-01-12 00:46:27 +0100

    Implement preview image conversion using imagemagick

--- test-in-qmlscene.sh
+++ test-in-qmlscene.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+previewResolutions=( 182 160 144 128 112 )
+webResolution=320
 PS3='
 Test a watchface in qmlscene.
 Enter watchface number.
@@ -13,11 +15,22 @@ done < <(find */ -maxdepth 0 -type d -print0 )
 if ! command -v qmlscene &> /dev/null
 then
     echo "
-$(tput setaf 1)qmlscene could not be found. Install qtcreator from your package manager.$(tput sgr0)"
+$(tput setaf 1)Qmlscene could not be found. Install qtcreator from your package manager.$(tput sgr0)"
     exit
 else
     echo "
-$(tput setaf 2)qmlscene found, proceeding...$(tput sgr0)
+$(tput setaf 2)Qmlscene found, proceeding...$(tput sgr0)
+         "
+fi
+
+if ! command -v magick &> /dev/null
+then
+    echo "
+$(tput setaf 1)Image magick could not be found. Install imagemagick from your package manager.$(tput sgr0)"
+    exit
+else
+    echo "
+$(tput setaf 2)Image magick found, proceeding...$(tput sgr0)
          "
 fi
 
@@ -34,6 +47,26 @@ do
             wget -O  background.jpg https://raw.githubusercontent.com/AsteroidOS/asteroid-wallpapers/master/full/000-flatmesh.jpg
         fi
         qmlscene ${opt::-1} loader.qml
+        if [[ -f "${opt::-1}-round.png" ]]
+        then
+            mogrify -resize $webResolutionx$webResolution -adaptive-sharpen 0x.4 -quality 85 -path .thumbnails -format webp ${opt::-1}-round.png
+            echo "$(tput setaf 2)Round preview found and converted to webp in .thumbnails folder.$(tput sgr0)"
+        fi
+        if [[ -f "${opt::-1}.png" ]]
+        then
+            mogrify -resize $webResolutionx$webResolution -adaptive-sharpen 0x.4 -quality 85 -path .thumbnails -format webp ${opt::-1}.png
+            echo "$(tput setaf 2)Square preview found and converted to webp in .thumbnails folder.$(tput sgr0)"
+        fi
+        if [[ -f "${opt::-1}-trans.png" ]]
+        then
+            echo "$(tput setaf 2)Transparent preview found.$(tput sgr0)"
+            for res in "${previewResolutions[@]}" ; do
+                mogrify -resize $resx$res -adaptive-sharpen 0x.8 -quality 70 -format png -path .watchfacespreview/$res ${opt::-1}-trans.png
+                echo "$res px resize done."
+            done
+            echo "$(tput setaf 2)Transparent previews generated successfully in .watchfacespreview.$(tput sgr0)"
+
+        fi
     else
       break
     fi
